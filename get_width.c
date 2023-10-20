9fef1dfdc4a270bf58809318a18ca52ee4afed04
CharityZawadi <zawadicharity8@gmail.com>
2023-10-20 14:28:34 -0700

    get_width.c

--- get_width.c
+++ get_width.c
@@ -1,36 +1,85 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * get_width - Calculate the width for printing
- * @format: Formatted string for printing arguments
- * @i: Current index in the format
- * @list: List of arguments
+ * init_width - Initialize width to its default value.
+ * @width: A pointer to the Width struct to be initialized.
  *
- * Return: Width
+ * This function sets the specified flag to 0 and the value to 0.
  */
-int get_width(const char *format, int *i, va_list list)
+void init_width(Width *width)
 {
-	int curr_i;
-	int width = 0;
-
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return width;
+width->specified = 0;
+width->value = 0;
 }
+
+/**
+ * parse_width - Parse and set the width based on the format specifier.
+ * @format: The format specifier to parse.
+ * @width: A pointer to the Width struct to store the parsed width.
+ *
+ * This function parses the
+ * format specifier to determine the width, if specified.
+ */
+void parse_width(const char *format, Width *width)
+{
+width->specified = 0;
+width->value = 0;
+
+while (*format >= '0' && *format <= '9')
+{
+width->specified = 1;
+width->value = width->value * 10 + (*format - '0');
+format++;
+}
+}
+
+/**
+ * get_width_value - Get the value of the specified width.
+ * @width: The Width struct containing the width value.
+ *
+ * Return: The value of the specified width.
+ */
+int get_width_value(Width width)
+{
+return (width.value);
+}
+
+/**
+ * is_width_specified - Check if width is specified.
+ * @width: The Width struct to check.
+ *
+ * Return: 1 if width is specified, 0 otherwise.
+ */
+int is_width_specified(Width width)
+{
+return (width.specified);
+}
+
+/**
+ * main - Entry point for testing the printf implementation.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+int len;
+
+len = _printf("Let's try to printf a simple sentence.\n");
+printf("Length:[%d]\n", len);
+
+len = _printf("Character:[%c]\n", 'H');
+printf("Length:[%d]\n", len);
+
+len = _printf("String:[%s]\n", "I am a string !");
+printf("Length:[%d]\n", len);
+
+len = _printf("Hexadecimal:[%x]\n", 255);
+printf("Length:[%d]\n", len);
+
+len = _printf("Unknown:[%r]\n");
+printf("Length:[%d]\n", len);
+
+return (0);
+}
+
