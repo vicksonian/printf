4309ddb5a3eccac73b3d887480cbb6615beca5e8
CharityZawadi <zawadicharity8@gmail.com>
2023-10-20 15:06:44 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -1,26 +1,80 @@
 #include "main.h"
 
 /**
- * get_size - Calculate the size to cast the argument
- * @format: Formatted string for printing arguments
- * @i: Current index in the format
+ * init_size - Initialize size to its default value.
+ * @size: A pointer to the Size struct to be initialized.
  *
- * Return: Size specifier
+ * This function initializes the size modifier to NONE.
  */
-int get_size(const char *format, int *i)
+void init_size(Size *size)
 {
-    int curr_i = *i + 1;
-    int size = 0;
-
-    if (format[curr_i] == 'l')
-        size = S_LONG;
-    else if (format[curr_i] == 'h')
-        size = S_SHORT;
-
-    if (size == 0)
-        *i = curr_i - 1;
-    else
-        *i = curr_i;
-
-    return size;
+size->modifier = NONE;
 }
+
+/**
+ * parse_size - Parse and set the size modifier based on the format specifier.
+ * @format: The format specifier to parse.
+ * @size: A pointer to the Size struct to store the parsed size modifier.
+ *
+ * This function parses the format specifier to determine the size modifier.
+ * It supports 'h', 'hh', 'l', 'll', 'j', 'z', and other size modifiers.
+ */
+void parse_size(const char *format, Size *size)
+{
+size->modifier = NONE;
+
+if (*format == 'h')
+{
+size->modifier = SHORT;
+format++;
+if (*format == 'h')
+{
+size->modifier = CHAR;
+format++;
+}
+}
+else if (*format == 'l')
+{
+size->modifier = LONG;
+format++;
+if (*format == 'l')
+{
+size->modifier = LONG_LONG;
+format++;
+}
+}
+else if (*format == 'j')
+{
+size->modifier = INTMAX_T;
+format++;
+}
+else if (*format == 'z')
+{
+size->modifier = SIZE_T;
+format++;
+}
+
+}
+
+/**
+ * is_size_specified - Check if a size modifier is specified.
+ * @size: The Size struct to check.
+ *
+ * Return: 1 if a size modifier is specified, 0 otherwise.
+ */
+int is_size_specified(Size size)
+{
+return (size.modifier != NONE);
+}
+
+/**
+ * get_size_modifier - Get the specified size modifier.
+ * @size: The Size struct containing the size modifier.
+ *
+ * Return: The size modifier as a SizeModifier enum value.
+ */
+SizeModifier get_size_modifier(Size size)
+{
+return (size.modifier);
+}
+
