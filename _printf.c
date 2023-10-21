f2758fdd6e73353844e0cfaaeea9a968f3cc431f
George Michael Pussl <george.pussl@gmail.com>
2023-10-20 20:49:01 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,82 +1,56 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
-* print_char - Prints a character to standard output.
-* @args: The argument list containing the character to print.
-*
-* Return: The number of characters printed (always 1).
-*/
-int print_char(va_list args)
-{
-char c = va_arg(args, int);
-write(1, &c, 1);
-return (1);
-}
-
-/**
-* print_string - Prints a string to standard output.
-* @args: The argument list containing the string to print.
-*
-* Return: The number of characters printed (excluding the null byte).
-*/
-int print_string(va_list args)
-{
-char *str = va_arg(args, char *);
-
-if (str)
-{
-int char_count = 0;
-
-while (*str)
-{
-write(1, str, 1);
-str++;
-char_count++;
-}
-return (char_count);
-}
-return (0);
-}
-
-/**
-* print_integer - Prints an integer to standard output.
-* @args: The argument list containing the integer to print.
-*
-* Return: The number of characters printed.
-*/
-int print_integer(va_list args)
-{
-int num = va_arg(args, int);
-char num_str[12];
-int num_len = snprintf(num_str, sizeof(num_str), "%d", num);
-
-write(1, num_str, num_len);
-return (num_len);
-}
-
-/**
-* _printf - Produces output according to a format.
-* @format: Is a character string. The format string
-* is composed of zero or more directives.
-*
-* Return: The number of characters printed (excluding
-* the null byte used to end output to strings).
-*/
+ * _printf - Produces output according to a format.
+ * @format: A character string.
+ * The format string is composed of zero or more directives.
+ *
+ * Return: The number of characters printed
+ * (excluding the null byte used to end output to strings).
+ */
 int _printf(const char *format, ...)
+{	va_list args;
+	int char_count = 0;
+
+	if (format == NULL)
+	return (-1);
+	va_start(args, format);
+	while (*format)
 {
-va_list args;
-int char_count = 0;
+	if (*format == '%')
+	{format++;
+	if (*format == '\0')
+	break;
+	if (*format == 'c')
+	{char c = va_arg(args, int);
+	write(1, &c, 1);
+	char_count++;
+	}
+	else if (*format == 's')
+	{char *str = va_arg(args, char *);
+	if (str)
+	{int str_len = 0;
+	while (str[str_len])
+	str_len++;
+	write(1, str, str_len);
+	char_count += str_len;
+	}
+	}
+	else if (*format == '%')
+	{write(1, "%", 1);
+	char_count++;
+	}
+	}
+	else
+	{write(1, format, 1);
+	char_count++;
+	}
+	format++;
+	}
 
-if (format == NULL)
-return (-1);
-
-va_start(args, format);
-
-while (*format)
-{
+	va_end(args);
+	return (char_count);
 }
 
-va_end(args);
-return (char_count);
-}
--- main.h
+++ main.h
@@ -2,39 +2,8 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
-
-/**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
-
+int _printf(const char *format, ...);
 
 #endif
